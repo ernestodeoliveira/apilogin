1c33b48356c0647bbb7b9de1ce8490c22bf8ae78
Ernesto de Oliveira <ernestodeoliveira@gmail.com>
2023-06-26 21:53:32 -0300

    Diversas alterações

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST'])
+@app.route('/', methods=['POST'])
 def login():
     email = request.json.get('email')
     password = request.json.get('password')
@@ -12,5 +12,6 @@ def login():
     else:
         return jsonify({'message': 'Login não autorizado'}), 401
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
